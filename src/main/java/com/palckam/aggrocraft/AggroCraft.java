06f610841617c9253ccc70397c8fefb31a61c2a1
PalCkam <palckam07@gmail.com>
2026-07-14 17:55:53 +0200

    Ajout : enregistrement de SentinelEntity (sans rendu client)

--- src/main/java/com/palckam/aggrocraft/AggroCraft.java
+++ src/main/java/com/palckam/aggrocraft/AggroCraft.java
@@ -1,5 +1,6 @@
 package com.palckam.aggrocraft;
 
+import com.palckam.aggrocraft.entity.ModEntities;
 import net.fabricmc.api.ModInitializer;
 
 public class AggroCraft implements ModInitializer
@@ -8,6 +9,6 @@ public class AggroCraft implements ModInitializer
 	@Override
 	public void onInitialize()
 	{
-	
+		ModEntities.registerModEntities();
 	}
 }
---
+++ src/main/java/com/palckam/aggrocraft/entity/ModEntities.java
@@ -0,0 +1,26 @@
+package com.palckam.aggrocraft.entity;
+
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModEntities
+{
+	public static final EntityType<SentinelEntity> SENTINEL = Registry.register(
+			Registries.ENTITY_TYPE,
+			Identifier.of("aggrocraft", "sentinel"),
+			EntityType.Builder.create(SentinelEntity::new, SpawnGroup.CREATURE)
+					.dimensions(EntityDimensions.fixed(0.6f, 1.95f))
+					.build()
+	);
+	
+	
+	public static void registerModEntities()
+	{
+		FabricDefaultAttributeRegistry.register(SENTINEL, SentinelEntity.createSentinelAttributes());
+	}
+}
