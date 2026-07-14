2fe2027687b884142676b758d3dea97aa5378ac9
PalCkam <palckam07@gmail.com>
2026-07-14 17:15:50 +0200

    Ajout des attributs par défauts

--- src/main/java/com/palckam/aggrocraft/entity/SentinelEntity.java
+++ src/main/java/com/palckam/aggrocraft/entity/SentinelEntity.java
@@ -2,6 +2,8 @@ package com.palckam.aggrocraft.entity;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.world.World;
 
@@ -18,5 +20,12 @@ public class SentinelEntity extends PathAwareEntity
 	{
 		WanderAroundFarGoal goal = new WanderAroundFarGoal(this, 1.0D);
 		this.goalSelector.add(1, goal);
+	}
+	
+	public static DefaultAttributeContainer.Builder createSentinelAttributes()
+	{
+		return PathAwareEntity.createMobAttributes()
+				.add(EntityAttributes.GENERIC_MAX_HEALTH,     20.0D)
+				.add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.23D);
 	}
 }
